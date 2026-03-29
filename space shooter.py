182b6134fadf40cd0173cd473f1285b8bafab363
wtrip916-lgtm <wtrip916@gmail.com>
2026-03-29 22:40:09 +0530

    Add files via upload

---
+++ space shooter.py
@@ -0,0 +1,20 @@
+name = input("enter your name: ")
+print("hi " + name)
+age = int(input("enter your age: "))
+while True:
+    about_future = input("enter what you want to do in your life: ").strip()
+    if about_future != "":
+       print("work for your skills soon you achieve your goals")
+       break
+    else:
+       print("want to be a bagger")
+while True:
+    job = input("enter your job: ")
+    if job != "":
+        break
+    print("died now you are not exist in world")
+while True:
+    hobby = input("enter your hobby: ")
+    if hobby != "gaming":
+        break
+    print("not bad but it not make currier")
